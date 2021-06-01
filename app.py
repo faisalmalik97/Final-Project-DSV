419235e187bff6a2722e79bedf3f7b68fef35c85
Fasial Malik <74533041+faisalmalik97@users.noreply.github.com>
2021-06-01 20:43:16 +0800

    push

--- app.py
+++ app.py
@@ -6,35 +6,6 @@ from flask import Flask, jsonify, render_template, redirect, url_for
 # Flask Setup
 #################################################
 app = Flask(__name__)
-
-#################################################
-# Code
-#################################################
-import numpy as np
-import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-import datetime
-plt.style.use('bmh')
-from pandas import DataFrame
-from pandas import Grouper
-from matplotlib import pyplot
-from pandas import read_csv
-
-# #read the csv file
-# df = pd.read_csv('APT.csv',index_col="Date", parse_dates=True)
-# df.head()
-
-# # plot the historic data
-# plt.figure(figsize=(16,8))
-# plt.title('APT ASX Stock Price', fontsize = 18)
-# plt.xlabel('Days', fontsize= 18)
-# plt.ylabel('Close Price AUD ($)', fontsize = 18)
-# plt.plot(df['Close'])
-# plt.show()
-
 #################################################
 # Flask Routes
 #################################################
