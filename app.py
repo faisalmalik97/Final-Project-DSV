7a8d5798f50a5e4e2d4eea0af5cd8409b1313b3e
Fasial Malik <74533041+faisalmalik97@users.noreply.github.com>
2021-06-01 19:32:37 +0800

    push

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, redirect
+from flask import Flask, jsonify, render_template, redirect, url_for
 
 
 
@@ -16,6 +16,12 @@ app = Flask(__name__)
 @app.route("/")
 def echo():
     return render_template("index.html", text="Serving up cool text from the Flask server!!")
+
+@app.route("/addtwo/<number>")
+def addTwo(number):
+    newNumber = int(number)+2
+    return str(newNumber)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
